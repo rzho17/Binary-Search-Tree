e21672f69f1d9908adb41c4217ce2afcccf01fb7
Richard <richardzehungho@gmail.com>
2023-08-21 14:38:50 -0700

    Add eslint

---
+++ binary-search-tree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(root) {
+    this.root = root;
+  }
+}
