18572144d0878116e0b8663da839ef01eeabd08d
Richard <richardzehungho@gmail.com>
2023-08-22 14:37:21 -0700

    Add a build tree function to sort a sorted arr using BST

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,5 +1,31 @@
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const removeDuplicate = (arr) => {
+  const tempArr = arr.filter((data, index) => arr.indexOf(data) === index);
+
+  return tempArr;
+};
+
+const sortArray = (arr) => removeDuplicate(arr).sort((a, b) => a - b);
+
+// const test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(sortArray(test));
+
 class Node {
-  constructor(data, left, right) {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -8,6 +34,33 @@ class Node {
 
 class Tree {
   constructor(root) {
-    this.root = root;
+    this.root = this.buildTree(root);
+  }
+
+  buildTree(arr) {
+    const sorted = sortArray(arr);
+    const start = 0;
+    const end = sorted.length - 1;
+
+    if (start > end) return null;
+
+    const mid = Math.floor((0 + sorted.length - 1) / 2);
+
+    const node = new Node(sorted[mid]);
+
+    const left = sorted.slice(0, mid);
+    const right = sorted.slice(mid + 1, sorted.length);
+
+    node.left = this.buildTree(left);
+
+    node.right = this.buildTree(right);
+
+    return node;
   }
 }
+
+const t = new Tree(test);
+
+// console.log(t);
+
+prettyPrint(t.root);
