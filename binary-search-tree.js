cb12d34b3290e6a344108c36454008071f6b3e3d
Richard <richardzehungho@gmail.com>
2023-08-24 15:47:15 -0700

    Delete function works with leaf nodes, single child, and two childs

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -19,8 +19,8 @@ const removeDuplicate = (arr) => {
 
 const sortArray = (arr) => removeDuplicate(arr).sort((a, b) => a - b);
 
-// const test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 68];
+// const test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // console.log(sortArray(test));
 
@@ -60,17 +60,81 @@ class Tree {
 
   insert(root, key) {
     if (root === null) {
-      const node = new Node(key);
-      return node;
+      const root = new Node(key);
+      return root;
     }
 
     if (key < root.data) {
       root.left = this.insert(root.left, key);
-      // console.log("yo");
     } else if (key > root.data) {
       root.right = this.insert(root.right, key);
     }
 
+    return root;
+  }
+
+  minFinder(root, key) {
+    if (root === null) {
+      return root;
+    }
+
+    if (root.left !== null) {
+      root = this.minFinder(root.left, key);
+      return root;
+    }
+
+    // console.log(root);
+    return root;
+    // return root.left;
+  }
+
+  delete(root, key) {
+    if (root === null) {
+      return root;
+    }
+
+    // this.minFinder(root.right, key);
+
+    if (key < root.data) {
+      root.left = this.delete(root.left, key);
+    } else if (key > root.data) {
+      root.right = this.delete(root.right, key);
+    } else {
+      if (root.left === null && root.right === null) {
+        // return null;
+        return null;
+      } else if (root.left === null) {
+        return root.right;
+      } else if (root.right === null) {
+        return root.left;
+      } else {
+        // root = this.minFinder(root.right, key);
+
+        // console.log(root);
+
+        let temp = root;
+        temp = this.minFinder(root.right, key);
+        root.data = temp.data;
+
+        root.right = this.delete(root.right, temp.data);
+        console.log(temp);
+
+        // root = this.delete(root, temp.data);
+        // console.log(temp);
+
+        // console.log(root);
+
+        return root;
+      }
+    }
+
+    // if (root.left === null) {
+    //   // console.log(root);
+    //   // root = root.right;
+    //   return root.right;
+    // } else if (root.right === null) {
+    //   return root.left;
+    // }
     return root;
   }
 }
@@ -79,7 +143,11 @@ const t = new Tree(test);
 
 prettyPrint(t.root);
 
+// t.delete(t.root, 6);
+// prettyPrint(t.delete(t.root, 9));
+prettyPrint(t.delete(t.root, 67));
+prettyPrint(t.delete(t.root, 68));
+
+// t.minFinder(t.root, 9);
 // prettyPrint(t.insert(t.root, 0));
 // prettyPrint(t.insert(t.root, 100));
-// console.log(t.root);
-// prettyPrint(t.root);
